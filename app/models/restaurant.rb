1404df8655f69e4e069f8380c30153256ea465b6
Jose Wollner <zetowollner@gmail.com>
2023-02-24 15:01:15 -0300

    rails new

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  CATEGORIES =  ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, :inclusion=> { :in => CATEGORIES }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating restaurants...'
+
+Restaurant.create!(name:'Sushi Leblon', address:'Rua Dias Ferreira', phone_number:'971655670', category:'japanese')
+Restaurant.create!(name:'Rubaiyat', address:'Rua Jardim Botanico', phone_number:'971655670', category:'french')
+Restaurant.create!(name:'Pabu Izakaya', address:'Rua Dias Ferreira', phone_number:'971655670', category:'japanese')
+Restaurant.create!(name:'McDonalds', address:'Rua Visconde de Piraja', phone_number:'971655670', category:'chinese')
+Restaurant.create!(name:'Le Vin', address:'Avenida Ataulfo de Paiva', phone_number:'971655670', category:'french')
+
+puts 'Finished.'
